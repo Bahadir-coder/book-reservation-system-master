d86d7d8077f69c36c16be8e6b6bfe271c38cf8b8
Bahadir-coder <bahadurmusayev15@gmail.com>
2024-12-01 01:07:59 +0400

    Book Reservation Project

---
+++ BookReservation-master/src/main/java/com/example/bookreservation/dao/entity/DeletedEntity.java
@@ -0,0 +1,23 @@
+package com.example.bookreservation.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "deleted")
+public class DeletedEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookID;
+    private String bookName;
+    private String bookGenre;
+    private String bookCode;
+
+    public DeletedEntity() {
+    }
+}
